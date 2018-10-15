5e12e4626c077c738a910b6f588b4c5db7ba069f
Nathan Loden <nloden@neptunetg.com>
2018-10-15 09:19:04 -0500

    Fixed print syntax from p3 to p3

--- pipes.py
+++ pipes.py
@@ -13,7 +13,7 @@ def parent( ):
     pipein = open(pipe_name, 'r')
     while True:
         line = pipein.readline()[:-1]
-        print 'Parent %d got "%s" at %s' % (os.getpid(), line, time.time( ))
+        print('Parent %d got "%s" at %s' % (os.getpid(), line, time.time( )))
  
 if not os.path.exists(pipe_name):
     os.mkfifo(pipe_name)  
